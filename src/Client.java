2fb4a95292f6036eaa4e01bb6596cb89504f598e
Sierra <sierrasch@gmail.com>
2015-04-14 00:18:25 -0700

    Test commit, added print to Client.java

--- src/Client.java
+++ src/Client.java
@@ -3,5 +3,6 @@ import Client.DisplayFrame;
 public class Client {
 	public static void main(String[] args) {
 		DisplayFrame clientFrame = new DisplayFrame("foo");
+		System.out.println("Application exit.");
 	}
 }
